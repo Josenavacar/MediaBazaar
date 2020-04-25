158f77c92a7ba107b3d73c74a72bdf1eef8358dd
Cocks,Louis L.K <l.cocks@student.fontys.nl>
2020-04-25 20:11:43 +0200

    Finished the feature to search orders on order page

--- mediaBazaarStockManagement/view/orders/view.php
+++ mediaBazaarStockManagement/view/orders/view.php
@@ -1,9 +1,18 @@
         <div class="container-fluid">
             <h1 class="mt-4">Orders</h1>
 			<div class="table-responsive">
-				<table class="table table-hover">
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" id="myInput" placeholder="Search by order ID..">
+		            <span class="input-group-append">
+		                <span class="btn btn-secondary">
+		                    <i class="fa fa-search"></i>
+		                </span>
+		            </span>
+				</div>
+
+				<table class="table table-hover" id="myTable">
 					<thead class="table-primary">
-						<tr>
+						<tr class="header">
                             <th scope="col">Order ID</th>
 							<th scope="col">Name</th>
                             <th scope="col">Quantity</th>
@@ -14,7 +23,7 @@
 					<tbody>
 						<?php foreach ($orders as $order) { ?>
 							<tr>
-								<td><?php echo "OD_Nr". $order['OrderID']; ?></td>
+								<td><?php echo "OD-NR ". $order['OrderID']; ?></td>
                                 <td><?php echo $order['Name']; ?></td>
                                 <td><?php echo $order['Quantity']; ?></td>
 								<td><?php echo "€" . " " . $order['TotalPrice']; ?></td>
@@ -29,3 +38,25 @@
     <!-- /#page-content-wrapper -->
 </div>
 
+<script type="text/javascript">
+	document.getElementById("myInput").addEventListener("keyup", myFunction);
+
+	function myFunction() {
+	  var input, filter, table, tr, td, i, txtValue;
+	  input = document.getElementById("myInput");
+	  filter = input.value.toUpperCase();
+	  table = document.getElementById("myTable");
+	  tr = table.getElementsByTagName("tr");
+	  for (i = 0; i < tr.length; i++) {
+	    td = tr[i].getElementsByTagName("td")[0];
+	    if (td) {
+	      txtValue = td.textContent || td.innerText;
+	      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+	        tr[i].style.display = "";
+	      } else {
+	        tr[i].style.display = "none";
+	      }
+	    }       
+	  }
+	}
+</script>
